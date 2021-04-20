6fb58f3f334c0b0ba41e9a1fb70f305e5b6b084b
saiesaraf <saie.saraf@gmail.com>
2021-04-20 11:47:00 -0400

    Initial commit
    Add springboot java mongodb dependency

---
+++ backend/src/main/java/com/example/mentormenteewebsite/MentorMenteeWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.mentormenteewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorMenteeWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorMenteeWebsiteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/mentormenteewebsite/MentorMenteeWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mentormenteewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorMenteeWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
