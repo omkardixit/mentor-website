237e27d1f697bd6333dff3c1aa0d7da38e9cd494
Omkar <dixit.omkar@gmail.com>
2022-06-30 00:13:56 -0400

    fix-tests

--- src/test/java/com/example/mentormenteewebsite/MentorMenteeWebsiteApplicationTests.java
+++ src/test/java/com/example/mentormenteewebsite/MentorMenteeWebsiteApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.mentormenteewebsite;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes={com.example.mentormenteewebsite.MentorMenteeWebsiteApplication.class})
 class MentorMenteeWebsiteApplicationTests {
 
 	@Test
